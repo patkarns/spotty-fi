20067f6f435b23babeaa7136a612e82d9aadf746
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-07 23:06:14 -0800

    Add [setup]: routing wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './core/components/login/login.component';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent }
+];
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard {
+
+    constructor(private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (localStorage.getItem('token')) {
+            // logged in so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        return false;
+    }
+}
