ba50ebf3589ba1089264722f313c22543199c071
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-07 23:03:08 -0800

    Add [setup] : login component

---
+++ src/app/core/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginFacade } from '../../facades/login.facade';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  constructor(public loginFacade: LoginFacade) {}
+}
