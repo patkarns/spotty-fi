046f27818f3ce4adc6cc2bed510a81c756217300
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-09 13:16:42 -0800

    refactor [music-search]: clean up unnecessary imports, vars, and functions

--- src/app/core/components/tracks-display/tracks-display.component.ts
+++ src/app/core/components/tracks-display/tracks-display.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatRippleModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSliderModule } from '@angular/material/slider';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 import {
@@ -29,12 +27,8 @@ import {
 
 import { TracksFacade } from '../../facades/tracks.facade';
 import { PlaybackService } from '../../services/playback.service';
-import { SpotifyObject, Track } from '../../state/tracks';
+import { Track } from '../../state/tracks';
 import { GenreFacade } from '../../facades/genre.facade';
-import { getAvailableGenres } from '../../state/genre';
-import { FormsModule } from '@angular/forms';
-
-const defaultGenres = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"];
 
 @Component({
   selector: 'app-tracks-display',
@@ -42,15 +36,12 @@ const defaultGenres = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambie
   imports: [
     CommonModule,
     MatButtonModule,
-    MatCardModule,
     MatChipsModule,
     MatGridListModule,
     MatRippleModule,
     MatSliderModule,
-    FormsModule,
-    MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatFormFieldModule
   ],
   templateUrl: './tracks-display.component.html',
   styleUrl: './tracks-display.component.scss',
@@ -65,8 +56,6 @@ export class TracksDisplayComponent implements OnInit {
   public trackInfo: Track | null = null;
   public selectedGenre: string | null = null;
   public searchQuery: string = '';
-  // public selectedGenres: { [key: string]: boolean} = {};
-  // public selectedGenres: Map<string, boolean> = new Map();
   public popularGenresItems$: Observable<{ name: string; color: string }[]> =
     of([]);
 
@@ -83,11 +72,11 @@ export class TracksDisplayComponent implements OnInit {
     ]).pipe(
       switchMap(([artistsById, pageCount]) => {
         if (!keys(artistsById).length) {
-          const res = defaultGenres.slice(0, pageCount).map(genre => ({
-            name: genre,
-            color: this.chipColors.greenLightest
-          })) as { name: string; color: string }[];
-          return of(res);
+          // const res = defaultGenres.slice(0, pageCount).map(genre => ({
+          //   name: genre,
+          //   color: this.chipColors.greenLightest
+          // })) as { name: string; color: string }[];
+          return of([]);
         }
         this.genreFacade.getGenresByArtistIds(
           keys(artistsById).slice(0, pageCount)
@@ -113,23 +102,8 @@ export class TracksDisplayComponent implements OnInit {
     );
   }
 
-  // public onSearchBarUpdate(query: string) {
-  //   this.searchQuery = query;
-  // }
-  // public searchTracksByGenres() {
-  //   //  const genres = _map(filter(toPairs(this.selectedGenres), last), first);
-  //   this.tracksFacade.searchTracksByGenres(this.searchQuery, this.selectedGenre!);
-  // }
-
   public handleGenreToggle(genreName: string) {
     this.selectedGenre = (this.selectedGenre === genreName) ? null : genreName;
     if (this.selectedGenre) this.tracksFacade.searchTracksByGenre(this.selectedGenre);
-
   }
-
-  // public genreSelected(genreName: string) {
-  //   this.selectedGenre = this.selectedGenre === genreName ? null : genreName;
-  //   const selectedGenreState = this.selectedGenres.has(genreName) ? !this.selectedGenres.get(genreName) : true;
-  //   this.selectedGenres.set(genreName, selectedGenreState);
-  // }
 }
