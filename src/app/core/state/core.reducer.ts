748969c456b208857fd49696330bbf2ee88d3eab
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-09 15:25:52 -0800

    add [playback-queue]: add playback store to app

--- src/app/core/state/core.reducer.ts
+++ src/app/core/state/core.reducer.ts
@@ -3,11 +3,13 @@ import { State } from './core.state';
 import * as LoginReducer from './login/login.reducer';
 import * as TracksReducer from './tracks/tracks.reducer';
 import * as GenreReducer from './genre/genre.reducer';
+import * as PlaybackReducer from './playback/playback.reducer';
 
 export const reducers: ActionReducerMap<State> = {
   login: LoginReducer.reducer,
   tracks: TracksReducer.reducer,
-  genre: GenreReducer.reducer
+  genre: GenreReducer.reducer,
+  playback: PlaybackReducer.reducer
 };
 
 export const metaReducers: MetaReducer<State>[] = [];
--- src/app/core/state/core.state.ts
+++ src/app/core/state/core.state.ts
@@ -1,9 +1,11 @@
 import { Genre } from './genre';
 import { Login } from './login';
+import { Playback } from './playback';
 import { Tracks } from './tracks';
 
 export interface State {
   login: Login;
   tracks: Tracks;
   genre: Genre;
+  playback: Playback
 }
