8cc981332b56438e3dc3372e738aeafcefc44705
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-10 18:51:52 -0800

    add [playlist-functionality]: support adding tracks to playlist

--- src/app/core/components/playlists/playlists.component.ts
+++ src/app/core/components/playlists/playlists.component.ts
@@ -1,92 +1,107 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatRippleModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-
-import {
-  map as _map,
-  mean
-} from 'lodash';
 import {
   Observable,
   of,
   map,
+  take
 } from 'rxjs';
 
-import { Track } from '../../state/tracks';
 import { PlaylistsFacade } from '../../facades/playlists.facade';
-import { Playlist } from '../../state/playlists';
+import { Playlist } from '../../../shared/interfaces/state/playlist-interface';
 import { ScrollListComponent } from '../../../shared/components/scroll-list/scroll-list.component';
-import { ListItem } from '../../models/list-item.model';
+import { ListItem } from '../../../shared/interfaces/list-item.interface';
+import { TracksFacade } from '../../facades/tracks.facade';
 
 @Component({
   selector: 'app-playlists',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
     MatButtonModule,
-    MatChipsModule,
-    MatGridListModule,
     MatIconModule,
     MatRippleModule,
-    MatSliderModule,
-    MatInputModule,
-    MatFormFieldModule,
     MatProgressSpinnerModule,
     MatExpansionModule,
     BrowserAnimationsModule,
-    ScrollListComponent
+    ScrollListComponent,
+    MatSlideToggleModule,
   ],
   templateUrl: './playlists.component.html',
-  styleUrl: './playlists.component.scss'
+  styleUrl: './playlists.component.scss',
 })
 export class PlaylistsComponent implements OnInit {
-  public panelOpenState = false;
   public selectedPlaylist: Playlist | null = null;
   public playlistListItems$: Observable<ListItem[]> = of([]);
-  private playlistsById: { [key: string]: Playlist } = {};
   public playlistTrackListItems$: Observable<ListItem[]> = of([]);
-  private playlistTracksById: { [key: string]: Track } = {};
+  public isAddingToPlaylist: boolean = false;
+  private playlistsById: { [key: string]: Playlist } = {};
+  // private playlistTracksById: { [key: string]: Track } = {}; For deleting tracks from playlist (will be supported later)
 
-  constructor(public playlistsFacade: PlaylistsFacade)  {
-
-  }
+  constructor(
+    public playlistsFacade: PlaylistsFacade,
+    private tracksFacade: TracksFacade
+  ) {}
   public ngOnInit(): void {
-    this.playlistListItems$ = this.playlistsFacade.availablePlaylists$
-      .pipe(
-        map(availablePlaylists => availablePlaylists.map(availablePlaylist => ({
-          mainText: availablePlaylist.name,
-          subtitleText: availablePlaylist.owner.displayName || availablePlaylist.owner.id,
-          id: availablePlaylist.id
-        }) as ListItem))
-      );
-    
-    this.playlistTrackListItems$ = this.playlistsFacade.playlistTracks$.pipe(
-      map(playlistTracks => playlistTracks.map(playlistTrack => ({
-        mainText: playlistTrack.name,
-        subtitleText: playlistTrack.artists[0].name,
-        id: playlistTrack.id
-      }) as ListItem))
+    this.playlistListItems$ = this.playlistsFacade.availablePlaylists$.pipe(
+      map((availablePlaylists) =>
+        availablePlaylists.map(
+          (availablePlaylist) =>
+            ({
+              mainText: availablePlaylist.name,
+              subtitleText:
+                availablePlaylist.owner.displayName ||
+                availablePlaylist.owner.id,
+              id: availablePlaylist.id,
+            } as ListItem)
+        )
+      )
     );
-    
-    this.playlistsFacade.availablePlaylistsById$.subscribe(availablePlaylistsById => this.playlistsById = availablePlaylistsById);
-    this.playlistsFacade.playlistTracksById$.subscribe(playlistTracksById => this.playlistTracksById = playlistTracksById);
+
+    this.playlistTrackListItems$ = this.playlistsFacade.playlistTracks$.pipe(
+      map((playlistTracks) =>
+        playlistTracks.map(
+          (playlistTrack) =>
+            ({
+              mainText: playlistTrack.name,
+              subtitleText: playlistTrack.artists[0].name,
+              id: playlistTrack.id,
+              imageUrl: playlistTrack.imageUrl,
+            } as ListItem)
+        )
+      )
+    );
+
+    this.playlistsFacade.availablePlaylistsById$.subscribe(
+      (availablePlaylistsById) => (this.playlistsById = availablePlaylistsById)
+    );
+    // this.playlistsFacade.playlistTracksById$.subscribe(playlistTracksById => this.playlistTracksById = playlistTracksById);
   }
+
   public selectPlaylist(playlistId: string): void {
     this.playlistsFacade.getPlaylistTracksByPlaylistId(playlistId);
+    this.playlistsFacade.setSelectedPlaylistId(playlistId);
     this.selectedPlaylist = this.playlistsById[playlistId];
   }
 
+  public handleDeleteTrackFromPlaylist(trackId: string): void {
+    this.playlistsFacade.removeTracksFromPlaylist([trackId]);
+  }
+
+  public handleIsAddStateChange(e: MatSlideToggleChange): void {
+    this.playlistsFacade.setIsAddState(e.checked);
+    if (e.checked) {
+      this.tracksFacade.isLoaded$.pipe(take(1)).subscribe((isLoaded) => {
+        if (!isLoaded) this.tracksFacade.getUserSavedTracks();
+      });
+    }
+  }
 }
--- src/app/core/facades/playlists.facade.ts
+++ src/app/core/facades/playlists.facade.ts
@@ -3,28 +3,73 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import { getAvailablePlaylists, getAvailablePlaylistsById, getPlaylistTracks, getPlaylistTracksById, getIsLoading, getIsLoaded, getCount, getSearchOffset, getPageCount, getPageSize } from '../state/playlists/playlists.selector';
+import {
+  getAvailablePlaylists,
+  getAvailablePlaylistsById,
+  getPlaylistTracks,
+  getPlaylistTracksById,
+  getIsAddState,
+  getSelectedTracksById,
+  getSelectedPlaylistId,
+  getIsLoading,
+  getIsLoaded,
+  getCount,
+  getSearchOffset,
+  getPageCount,
+  getPageSize
+} from '../state/playlists/playlists.selector';
 import * as PlaylistsActions from '../state/playlists';
-import { Playlist } from '../state/playlists';
-import { Track } from '../state/tracks';
+import { Playlist } from '../../shared/interfaces/state/playlist-interface';
+import { Track } from '../../shared/interfaces/state/track-interface';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlaylistsFacade {
-    isLoading$: Observable<boolean> = this.store.select(getIsLoading);
-    isLoaded$: Observable<boolean> = this.store.select(getIsLoaded);
-    availablePlaylists$: Observable<Playlist[]> = this.store.select(getAvailablePlaylists);
-    availablePlaylistsById$: Observable<{ [key: string]: Playlist }> = this.store.select(getAvailablePlaylistsById);
-    playlistTracks$: Observable<Track[]> = this.store.select(getPlaylistTracks);
-    playlistTracksById$: Observable<{ [key: string]: Track }> = this.store.select(getPlaylistTracksById);
-    count$: Observable<number> = this.store.select(getCount);
-    pageCount$: Observable<number> = this.store.select(getPageCount);
-    pageSize$: Observable<number> = this.store.select(getPageSize);
-    searchOffset$: Observable<number> = this.store.select(getSearchOffset);
+  isLoading$: Observable<boolean> = this.store.select(getIsLoading);
+  isLoaded$: Observable<boolean> = this.store.select(getIsLoaded);
+  availablePlaylists$: Observable<Playlist[]> = this.store.select(
+    getAvailablePlaylists
+  );
+  availablePlaylistsById$: Observable<{ [key: string]: Playlist }> =
+    this.store.select(getAvailablePlaylistsById);
+  playlistTracks$: Observable<Track[]> = this.store.select(getPlaylistTracks);
+  playlistTracksById$: Observable<{ [key: string]: Track }> = this.store.select(
+    getPlaylistTracksById
+  );
+  count$: Observable<number> = this.store.select(getCount);
+  pageCount$: Observable<number> = this.store.select(getPageCount);
+  pageSize$: Observable<number> = this.store.select(getPageSize);
+  searchOffset$: Observable<number> = this.store.select(getSearchOffset);
+  isAddState$: Observable<boolean> = this.store.select(getIsAddState);
+  selectedPlaylistId$: Observable<string> = this.store.select(
+    getSelectedPlaylistId
+  );
+  selectedTracksById$: Observable<{ [key: string]: Track }> = this.store.select(
+    getSelectedTracksById
+  );
+//   trackIdsToRemove$: Observable<string[]> = this.store.select(getTrackIdsToRemove);
 
-    constructor(private store: Store) {}
+  constructor(private store: Store) {}
 
-    getUserPlaylists = () => this.store.dispatch(PlaylistsActions.getUserPlaylists());
-    getPlaylistTracksByPlaylistId = (playlistId: string) => this.store.dispatch(PlaylistsActions.getPlaylistTracksByPlaylistId( {playlistId }));
+  addTracksToPlaylist = () =>
+    this.store.dispatch(PlaylistsActions.addTracksToPlaylist());
+  removeTracksFromPlaylist = (trackIds: string[]) =>
+    this.store.dispatch(PlaylistsActions.removeTracksFromPlaylist({ trackIds }));
+  setSelectedPlaylistId = (selectedPlaylistId: string) =>
+    this.store.dispatch(
+      PlaylistsActions.setSelectedPlaylistId({ selectedPlaylistId })
+    );
+  setIsAddState = (isAddState: boolean) =>
+    this.store.dispatch(PlaylistsActions.setIsAddState({ isAddState }));
+  setSelectedTracksById = (selectedTracksById: { [key: string]: Track }) =>
+    this.store.dispatch(
+      PlaylistsActions.setSelectedTracksById({ selectedTracksById })
+    );
+  getUserPlaylists = () =>
+    this.store.dispatch(PlaylistsActions.getUserPlaylists());
+  getPlaylistTracksByPlaylistId = (playlistId: string) =>
+    this.store.dispatch(
+      PlaylistsActions.getPlaylistTracksByPlaylistId({ playlistId })
+    );
 }
