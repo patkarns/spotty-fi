702384ec142cafff5801805eb65cfbe7022b31db
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-10 18:40:22 -0800

    refactor [playlist-functionality]: use scroll list component to display queue

--- src/app/core/components/queue/queue.component.ts
+++ src/app/core/components/queue/queue.component.ts
@@ -1,29 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
+
+import { first } from 'lodash';
+import {
+  map, Observable, of,
+} from 'rxjs';
 
 import { PlaybackFacade } from '../../facades/playback.facade';
+import { ScrollListComponent } from '../../../shared/components/scroll-list/scroll-list.component';
+import { ListItem } from '../../../shared/interfaces/list-item.interface';
 
 @Component({
   selector: 'app-queue',
   standalone: true,
-  imports: [CommonModule, MatListModule],
+  imports: [CommonModule, ScrollListComponent],
   templateUrl: './queue.component.html',
   styleUrl: './queue.component.scss'
 })
 export class QueueComponent implements OnInit {
-  public queueItemColor: { [key: number]: string } = {
-    0: '#f9fcf5',
-    1: '#ffffff'
-  }
+  public queueListItems$: Observable<ListItem[]> = of([]);
 
-  constructor(public playbackFacade: PlaybackFacade) {
-
-  }
+  constructor(public playbackFacade: PlaybackFacade) {}
 
   public ngOnInit(): void {
     this.playbackFacade.getUserQueue();
-    this.playbackFacade.queue$.subscribe(queue => console.log('queue', queue))
+    this.queueListItems$ = this.playbackFacade.queue$.pipe(
+      map(tracksInQueue => tracksInQueue.map(trackInQueue => ({
+        mainText: trackInQueue.name,
+        subtitleText: first(trackInQueue.artists)?.name,
+        id: trackInQueue.id,
+        imageUrl: trackInQueue.imageUrl
+      }) as ListItem)))
   }
 
 }
+
