8490a084d98ecf6954c89ae3642a5664fa1eedfc
patkarns <30379111+patkarns@users.noreply.github.com>
2024-02-10 18:45:48 -0800

    refactor [playlist-functionality]: revise responses from retrieveToken()

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,39 +3,41 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
 
-interface AccessToken {
+interface RetrieveTokenResponse {
   access_token: string;
+  token_expiry_utc: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private isAuthenticated = false;
-
   constructor(private http: HttpClient) {}
 
-  retrieveToken(): Observable<boolean> {
+  public retrieveToken(): Observable<{
+    accessToken: string;
+    expiryDate: string;
+    isTokenExpired: boolean;
+  }> {
     const getAuthTokenUrl = '/auth/token';
-    return (
-      this.http.get<AccessToken>(getAuthTokenUrl).pipe(
-        map(response => {
-        const tokenString = response.access_token;
-        localStorage.setItem('token', tokenString);
-        this.isAuthenticated = tokenString.length > 0;
-        return this.isAuthenticated;
-        })
-      )
-    );    
+    return this.http.get<RetrieveTokenResponse>(getAuthTokenUrl).pipe(
+      map((response) => {
+        const accessToken = response.access_token;
+        const expiryDate = response.token_expiry_utc;
+        return {
+          accessToken,
+          expiryDate,
+          isTokenExpired: dayjs().isAfter(expiryDate),
+        };
+      })
+    );
   }
 
-  logout(): void {
-    localStorage.removeItem('token');
-    this.isAuthenticated = false;
-  }
-
-  getToken(): string | null {
-    return localStorage.getItem('token');
+  public logout(): void {
+    // TODO
   }
 }
